25a0af4af4d15d0ed1407308daa4765649da0d16
Hamada Mohammed Seif <hamada.devlop@gmail.com>
2022-11-06 14:40:09 +0200

    binding adapters

--- app/src/main/java/com/example/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/example/asteroidradar/main/MainFragment.kt
@@ -1,15 +1,17 @@
 package com.example.asteroidradar.main
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.asteroidradar.R
 import com.example.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,7 +19,20 @@ class MainFragment : Fragment() {
         val binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
+        binding.viewModel = viewModel
+
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return true
     }
 
 }
